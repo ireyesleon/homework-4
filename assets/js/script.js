1bb199f1c958a6a3a79d6f15af46b13336089d2b
ireyesleon <izaaiaz@gmail.com>
2021-11-28 19:08:34 -0600

    added the questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,55 @@ var questions = document.querySelector(".question-title");
 var answers = document.querySelector(".answers");
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
-
 var secondsLeft = 75;
+
+var myQuestions = [
+	{
+		question: "What is the capital of Germany?",
+		answers: {
+			a: 'Munich',
+			b: 'Berlin',
+			c: 'Frankfurt'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is the capital of Spain",
+		answers: {
+			a: 'Barcelona',
+			b: 'Ibiza',
+			c: 'Madrid'
+		},
+		correctAnswer: 'c'
+	}
+  {
+		question: "What is capital of Poland?",
+		answers: {
+			a: 'Kraków',
+			b: 'Warzaw',
+			c: 'Lublin'
+		},
+		correctAnswer: 'b'
+	}
+  {
+		question: "What is the capital of Iceland?",
+		answers: {
+			a: 'Reykjavík',
+			b: 'Kópavogur',
+			c: 'Hafnarfjörður'
+		},
+		correctAnswer: 'a'
+	}
+  {
+		question: "What is the capital of Latvia?",
+		answers: {
+			a: 'Liepāja',
+			b: 'Riga',
+			c: 'Jūrmala'
+		},
+		correctAnswer: 'b'
+	}
+];
 
 //function for the countdown
 document.getElementById("start-button").addEventListener("click", function setTime() {
@@ -21,14 +68,16 @@ document.getElementById("start-button").addEventListener("click", function setTi
   })
 
 function sendMessage() {
-    timeEl.textContent = " ";
+    timeEl.textContent = "Time remaining: 0";
     var gameOver = document.createElement("img");
-    gameOver.setAttribute("src", "images/image_1.jpg");
+    gameOver.setAttribute("src", "assets/images/image_1.jpg");
     mainEl.appendChild(gameOver);
   
   }
+
+
   
-  setTime();
+  // setTime();
 
 //function to move to the first question
 
