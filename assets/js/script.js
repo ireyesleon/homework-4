dc7da44c6eecce901252d550d7648660b0817469
ireyesleon <izaaiaz@gmail.com>
2021-11-30 17:47:43 -0600

    Added questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,69 @@
 //var to appear in the question and answers
 var questions = document.querySelector(".question-title");
-var answers = document.querySelector(".answers");
+var answersContainer = document.querySelector(".answers-container");
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
+var startBtn = document.getElementById("start-button");
+var submitBtn = document.getElementById("submit-button")
 var secondsLeft = 75;
+var timerInterval;
+var initials = document.getElementById("input")
+var startPage = document.getElementById("start-page");
+
+var currentQuestionIndex = 0;
+var score = 0;
 
 var myQuestions = [
 	{
 		question: "What is the capital of Germany?",
-		answers: {
-			a: 'Munich',
-			b: 'Berlin',
-			c: 'Frankfurt'
-		},
+		answers: [
+			'Munich',
+			'Berlin',
+			'Frankfurt'
+		],
 		correctAnswer: 'b'
 	},
 	{
 		question: "What is the capital of Spain",
-		answers: {
-			a: 'Barcelona',
-			b: 'Ibiza',
-			c: 'Madrid'
-		},
+		answers: [
+			'Barcelona',
+			'Ibiza',
+			'Madrid'
+		],
 		correctAnswer: 'c'
-	}
+	},
   {
 		question: "What is capital of Poland?",
-		answers: {
-			a: 'Kraków',
-			b: 'Warzaw',
-			c: 'Lublin'
-		},
+		answers: [
+			'Kraków',
+			'Warzaw',
+			'Lublin'
+		],
 		correctAnswer: 'b'
-	}
+	},
   {
 		question: "What is the capital of Iceland?",
-		answers: {
-			a: 'Reykjavík',
-			b: 'Kópavogur',
-			c: 'Hafnarfjörður'
-		},
+		answers: [
+			'Reykjavík',
+			'Kópavogur',
+			'Hafnarfjörður'
+		],
 		correctAnswer: 'a'
-	}
+		},
   {
 		question: "What is the capital of Latvia?",
-		answers: {
-			a: 'Liepāja',
-			b: 'Riga',
-			c: 'Jūrmala'
-		},
+		answers: [
+			'Liepāja',
+			'Riga',
+			'Jūrmala'
+		],
 		correctAnswer: 'b'
 	}
 ];
 
-//function for the countdown
-document.getElementById("start-button").addEventListener("click", function setTime() {
-    var timerInterval = setInterval(function() {
+function setTime() {
+    timerInterval = setInterval(function() {
+	
       secondsLeft--;
       timeEl.textContent = "Time remaining: " + secondsLeft;
   
@@ -65,22 +73,79 @@ document.getElementById("start-button").addEventListener("click", function setTi
       }
   
     }, 1000);
-  })
+};
 
 function sendMessage() {
     timeEl.textContent = "Time remaining: 0";
-    var gameOver = document.createElement("img");
-    gameOver.setAttribute("src", "assets/images/image_1.jpg");
-    mainEl.appendChild(gameOver);
-  
-  }
-
+	questions.textContent = "Game Over! Enter your initials below";
+	answers.textContent = "";
+	start.textContent = "Try again";
+	var initials = document.createElement("input");
+	answers.appendChild(initials);
+	start.addEventListener("click", function tryAgain(){
+		window.location.reload();
+	})
+  };
 
   
   // setTime();
 
 //function to move to the first question
+function startQuestions() {
+	var currentQuestion = myQuestions[currentQuestionIndex];
+	questions.textContent = currentQuestion.question;
+	answersContainer.innerHTML = "";
+	currentQuestion.answers.forEach(function(answer) {
+		var answerBtn = document.createElement("button");
+		answersContainer.appendChild(answerBtn);
+		answerBtn.innerHTML = answer;
+		answerBtn.onclick = pressBtn;
+	})
+}
 
+function pressBtn() {
+	if (this.value !== myQuestions[currentQuestionIndex].answers) {
+		secondsLeft -= 5;
+		if (secondsLeft < 0) {
+			secondsLeft = 0
+		}
+		timerInterval.textContent = secondsLeft;
+	} 
 
+	currentQuestionIndex++
+	score += 1; // make it show in the html
 
-// clic event to move to the first question
+	if (currentQuestionIndex === myQuestions.length) {
+		endGame(); 
+	} else {
+		startQuestions();
+	}
+}
+
+//end game function **this is important
+function endGame() {
+	questions.textContent = "You got " + score + " out of 5";
+	answersContainer.innerHTML = "Enter your initials";
+	var setInitials = document.createElement("input");
+		answersContainer.appendChild(setInitials);
+	var submitBtn = document.createElement("button");
+		submitBtn.textContent = "Submit!";
+		submitBtn.setAttribute("class", "submit-button")
+		answersContainer.appendChild(submitBtn);
+		clearInterval(timerInterval);
+		// submitBtn.addEventListener("click", function() {
+		// 	localStorage.setItem("Initials", initials);
+		// 	localStorage.setItem("Score", score);
+		// })
+}
+//save highscores with local storage
+
+// submitBtn.addEventListener("click", function() {
+// 	localStorage.setItem("Initials", initials);
+// })
+
+startBtn.addEventListener("click", function() {
+	setTime();
+	startPage.setAttribute("class", "hidden");
+	startQuestions();
+})
